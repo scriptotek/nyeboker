4b21d891c59b1f6a6ce4c8094fa0cd74e3764e06
stianlagstad <stianlagstad@gmail.com>
2014-07-14 11:07:43 +0200

    fixed a wrongly placed )

--- app/routes.php
+++ app/routes.php
@@ -20,13 +20,13 @@ Route::get('/', function() {
 });
 
 // API ROUTES
-Route::group(array('prefix' => 'api', function () {
+Route::group(array('prefix' => 'api'), function () {
 	// since we will be using this just for CRUD, we won't need create and
 	// edit. Angular will handle both of those forms. This ensures that a user
 	// can't access api/create or api/edit when there's nothing there
 	Route::resource('comments', 'CommentController',
 		array('only' => array('index', 'store', 'destroy')));
-}));
+});
 
 // CATCH ALL ROUTE
 // Al routes that are not home or api will be redirected to the frontend. This
