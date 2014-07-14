222c6ae76c004e8c5aac31af2054256923bdb0fb
stianlagstad <stianlagstad@gmail.com>
2014-07-14 10:48:57 +0200

    db seed comments table

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		// $this->call('UserTableSeeder');
+		
+		$this->call('CommentTableSeeder');
+		$this->command->info('Comment table seeded.');
 	}
 
 }
