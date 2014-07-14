59563dd066cd898d18494bd442c25dba2abd4d99
stianlagstad <stianlagstad@gmail.com>
2014-07-14 11:05:21 +0200

    laravel routes in order

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,26 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// HOME PAGE
+Route::get('/', function() {
+	// we don't need to use Laravel Blade. We will return a PHP file that will
+	// hold all of our Angular content. See the "Where to Place Angular Files"
+	// below to see ideas on how to structure your app
+	return View::make('index'); // will return app/views/index.php
+});
+
+// API ROUTES
+Route::group(array('prefix' => 'api', function () {
+	// since we will be using this just for CRUD, we won't need create and
+	// edit. Angular will handle both of those forms. This ensures that a user
+	// can't access api/create or api/edit when there's nothing there
+	Route::resource('comments', 'CommentController',
+		array('only' => array('index', 'store', 'destroy')));
+}));
+
+// CATCH ALL ROUTE
+// Al routes that are not home or api will be redirected to the frontend. This
+// allows angular to route them.
+App::missing(function($exception) {
+	return View::make('index');
 });
