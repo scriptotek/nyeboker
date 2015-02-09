38505cb7912928dfb04e059011d00b803b39c86e
stianlagstad <stianlagstad@gmail.com>
2015-02-09 14:03:33 +0100

    test

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => $_ENV['mysql-database'],
-			'username'  => $_ENV['mysql-username'],
-			'password'  => $_ENV['mysql-password'],
+			'database'  => getenv('mysql-database'),
+			'username'  => getenv('mysql-username'),
+			'password'  => getenv('mysql-password'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
